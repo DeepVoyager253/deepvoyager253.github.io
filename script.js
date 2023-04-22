f36290fa20eb2a21c9d3a86f0c29f9df351a2e44
eyal.br <68502445+DeepVoyager253@users.noreply.github.com>
2023-04-22 09:03:34 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var timer_element = document.getElementById("Countdown");
+
+function get_time_until() {
+    var date_now = new Date();
+    var date_future = new Date('2023-5-12')
+    var delta = Math.abs(date_future - date_now) / 1000;
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+    var minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+    var seconds = delta % 60;
+
+    return [days, hours, minutes, seconds]
+}
+function update_timer(diff) {
+    timer_element.innerHTML = diff.join(':')
+    setTimeout(update_timer, 1000)
+
+}
